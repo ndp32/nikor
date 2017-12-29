dcd1bf3b4ab7aca84f48e0c78aba9db154b7700e
ndp32 <ndp32@georgetown.edu>
2017-12-29 13:49:32 -0500

    added theme_ts

--- R/theme_ts.R
+++ R/theme_ts.R
@@ -11,21 +11,22 @@
 #' @importFrom ggplot2 element_blank
 #' @export
 theme_ts <- function(base_size = 12, base_family = "serif") {
-  theme_minimal(base_size = base_size, base_family = base_family) %+replace%
+  theme_minimal(base_size = base_size, base_family = base_family) +
     theme(
-      line                 = element_line(color = "grey40", lineend = "round"),
-      text                 = element_text(family = "serif", color = "grey40"),
+      line                 = element_line(color = "grey50", lineend = "round"),
+      text                 = element_text(family = base_family, color = "grey40"),
       panel.grid.major.x   = element_blank(),
       panel.grid.minor.x   = element_blank(),
+      panel.grid.major.y   = element_line(color = "grey90", linetype = 3),
       panel.grid.minor.y   = element_blank(),
-      panel.background     = element_rect(fill = "white", color = "grey80"),
-      plot.background      = element_rect(fill = "grey80", color = "grey80"),
+      panel.background     = element_rect(fill = "white", color = "grey90"),
+      plot.background      = element_rect(fill = "grey90", color = "grey90"),
       plot.margin          = margin(10, 35, 15, 10),
-      axis.line.x          = element_line(color = "grey60", lineend = "round"),
+      axis.line.x          = element_line(color = "grey50", lineend = "round"),
       axis.title.x         = element_blank(),
       axis.text.x          = element_text(hjust = 0),
-      axis.ticks.x         = element_line(color = "grey60", lineend = "round"),
-      legend.position      = "right",
+      axis.ticks.x         = element_line(color = "grey50", lineend = "round"),
+      legend.position      = "top",
       legend.justification = "center",
       legend.margin        = margin(0, 0, 0, 0),
       legend.box.margin    = margin(-5, -5, -5, -5),
